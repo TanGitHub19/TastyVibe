85a3260a08bde7d536bed620f836600c7fd885df
MarkTristanRaroque <145894944+TanGitHub19@users.noreply.github.com>
2025-03-22 11:24:04 +0800

    For testing

--- backend/spotifyConfig.js
+++ backend/spotifyConfig.js
@@ -3,8 +3,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const SPOTIFY_CONFIG = {
-  clientId: process.env.SPOTIFY_CLIENT_ID,
-  clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-  redirectUri: process.env.SPOTIFY_REDIRECT_URI,
+  clientId: "fc681f27f67940dfbf8d181d263491e5",
+  clientSecret: "9572ad220a6f49ebb50220276135a4ac",
+  SPOTIFY_REDIRECT_URI:"https://tasty-vibe.onrender.com/api/spotify/callback",
 };
 
